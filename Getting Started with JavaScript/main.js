bb866969c926deb9c32c7d08b087b913a1e9251c
IsaqueuAfonso <122176251+IsaqueuAfonso@users.noreply.github.com>
2024-06-21 19:27:23 -0300

    Basic tests with JS

---
+++ Getting Started with JavaScript/main.js
@@ -0,0 +1,68 @@
+// alert("Hello World")
+
+// let NumberOne = 10
+// let NumberTwo = 10
+// alert(NumberOne + NumberTwo)
+/* */
+/* */
+// let controlVariable = "Olá"
+// let comparison = typeof controlVariable === "number"
+
+// if (comparison) {
+//   alert("É um número")
+// } else {
+//   alert("Não é um número")
+// }
+/* */
+/* */
+// let controlVariable = "Olá"
+// let comparison = typeof controlVariable === "string"
+
+// if (comparison) {
+//   alert("É uma string")
+// } else {
+//   alert("Não é uma string")
+// }
+/* */
+/* */
+// let controlVariable = true
+// let comparison = typeof controlVariable === "boolean"
+
+// if (comparison) {
+//   alert("É um booleano")
+// } else {
+//   alert("Não é um booleano")
+// }
+/* */
+/* */
+// let NumberOne = 10
+// let NumberTwo = 5
+// alert(NumberOne - NumberTwo)
+/* */
+/* */
+// let NumberOne = 10
+// let NumberTwo = 5
+// alert(NumberOne * NumberTwo)
+/* */
+/* */
+// let NumberOne = 10
+// let NumberTwo = 5
+// alert(NumberOne / NumberTwo)
+/* */
+/* */
+// let Number = 20
+// let evenOrOdd = Number % 2 === 0
+// if (evenOrOdd) {
+//   alert("É um número par")
+// } else {
+//   alert("Não é um número par")
+// }
+/* */
+/* */
+let Number = 11
+let evenOrOdd = Number % 2 === 1
+if (evenOrOdd) {
+  alert("É um número ímpar")
+} else {
+  alert("Não é um número ímpar")
+}
